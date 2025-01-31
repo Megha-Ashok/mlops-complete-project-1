9a3862abe08d8af8454738de5bbbd6f9c8925b74
Megha <megharashokashok@gmail.com>
2025-01-31 20:15:56 +0530

    data ingestion added

--- demo.py
+++ demo.py
@@ -10,6 +10,7 @@ logging.critical("This is a critical message.")
 '''
 
 # # below code is to check the exception config
+'''
 from src.logger import logging
 from src.exception import MyException
 import sys
@@ -19,3 +20,8 @@ try:
 except Exception as e:
     logging.info(e)
     raise MyException(e, sys) from e
+'''
+from src.pipline.training_pipeline import TrainPipeline
+
+pipline = TrainPipeline()
+pipline.run_pipeline()
